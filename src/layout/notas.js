836b18878b277d022ac8d01bf876e04960dbbe06
Sebastian Sarmiento <sebi_sarmiento@hotmail.com>
2018-11-24 12:32:21 -0300

    Mobile view

--- src/layout/notas.js
+++ src/layout/notas.js
@@ -8,7 +8,8 @@ class Notas extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      gastronomia: 'current-categoria'
+      gastronomia: 'current-categoria',
+      showCategorias: false
     }
     this.getData = this.getData.bind(this)
   }
@@ -18,14 +19,14 @@ class Notas extends Component {
     this.getData(this.props.location.pathname)
   }
 
-  getData(url){
-    this.setState({loadingData: true})
+  getData(url) {
+    this.setState({ loadingData: true })
     console.log(url)
     fetch(`http://localhost:3000${url}`)
       .then(d => d.json())
       .then(res => {
         console.log(res)
-        this.setState({ data: res, loadingData: false})
+        this.setState({ data: res, loadingData: false })
       })
       .catch(err => {
         console.log(err)
@@ -50,16 +51,18 @@ class Notas extends Component {
     return (
       <div className="notas-container">
         <div className="notas-categoria-columna" >
-          <h3>Categorias:</h3>
-          <Link className={this.state.gastronomia} name="gastronomia" onClick={e => this.handleClick(e)} to="../notas/gastronomia" >Gastronomia</Link>
-          <Link className={this.state.turismo} name="turismo" onClick={e => this.handleClick(e)} to="../notas/turismo" >Turismo</Link>
-          <Link className={this.state.cultura} name="cultura" onClick={e => this.handleClick(e)} to="../notas/cultura" >Cultura</Link>
-          <Link className={this.state.vinos} name="vinos" onClick={e => this.handleClick(e)} to="../notas/vinos" >Vinos</Link>
-          <Link className={this.state.otro} name="otro" onClick={e => this.handleClick(e)} to="../notas/otro" >Otros</Link>
+          <h3>Categorias <i onClick={() => this.setState({showCategorias: !this.state.showCategorias})} className="fas fa-plus"></i></h3>
+          <div className={this.state.showCategorias ? 'notas-categorias-mobile' : 'notas-categorias'} >
+            <Link className={this.state.gastronomia} name="gastronomia" onClick={e => this.handleClick(e)} to="../notas/gastronomia" >Gastronomia</Link>
+            <Link className={this.state.turismo} name="turismo" onClick={e => this.handleClick(e)} to="../notas/turismo" >Turismo</Link>
+            <Link className={this.state.cultura} name="cultura" onClick={e => this.handleClick(e)} to="../notas/cultura" >Cultura</Link>
+            <Link className={this.state.vinos} name="vinos" onClick={e => this.handleClick(e)} to="../notas/vinos" >Vinos</Link>
+            <Link className={this.state.otro} name="otro" onClick={e => this.handleClick(e)} to="../notas/otro" >Otros</Link>
+          </div>
         </div>
         <div className="notas-container-notas" >
           {
-            this.state.data ? this.state.data.map(nota => 
+            this.state.data ? this.state.data.map(nota =>
               <Nota _id={nota._id}
                 categoria={nota.categoria}
                 key={nota._id}
